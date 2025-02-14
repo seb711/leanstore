e565572a395e85aeebb7e161e474e0683b9eb822
kosakseb <sebastian.kosak@tum.de>
2025-02-14 15:17:16 +0100

    Modified YCSB benchmark to work with osv_task_enqueue.
    Modifications:
    - the overall execution model changed; before the modification we just ran the tasks sequentially. The worker threads could only run one task at a time and therefore we run them in serial order
    - now we push it in the runqueue of the task-worker (this happens async); so just taking the model from before would be problematic -> therefore i add a sleep to just test if the execution is successful and to have a working prototype

--- frontend/ycsb/ycsb.cpp
+++ frontend/ycsb/ycsb.cpp
@@ -13,8 +13,10 @@
 #include <gflags/gflags.h>
 #include <tbb/parallel_for.h>
 // -------------------------------------------------------------------------------------
+#include <osv/task.h>
 #include <iostream>
 #include <set>
+
 // -------------------------------------------------------------------------------------
 DEFINE_uint32(ycsb_read_ratio, 100, "");
 DEFINE_uint64(ycsb_tuple_count, 0, "");
@@ -48,9 +50,9 @@ int main(int argc, char** argv)
    // -------------------------------------------------------------------------------------
    // Always init with the maximum number of threads (FLAGS_worker_threads)
    LeanStore db;
-   auto& crm = db.getCRManager();
+   // auto& crm = db.getCRManager();
    LeanStoreAdapter<KVTable> table;
-   crm.scheduleJobSync(0, [&]() { table = LeanStoreAdapter<KVTable>(db, "YCSB"); });
+   table = LeanStoreAdapter<KVTable>(db, "YCSB");
    db.registerConfigEntry("ycsb_read_ratio", FLAGS_ycsb_read_ratio);
    db.registerConfigEntry("ycsb_threads", FLAGS_ycsb_threads);
    db.registerConfigEntry("ycsb_ops_per_tx", FLAGS_ycsb_ops_per_tx);
@@ -88,55 +90,65 @@ int main(int argc, char** argv)
       return 0;
    }
    // -------------------------------------------------------------------------------------
-   if (FLAGS_recover) {
-      // Warmup
-      if (FLAGS_ycsb_warmup) {
-         cout << "Warmup: Scanning..." << endl;
-         {
-            begin = chrono::high_resolution_clock::now();
-            utils::Parallelize::range(FLAGS_worker_threads, n, [&](u64 t_i, u64 begin, u64 end) {
-               crm.scheduleJobAsync(t_i, [&, begin, end]() {
-                  for (u64 i = begin; i < end; i++) {
-                     YCSBPayload result;
-                     // cr::Worker::my().startTX(tx_type, isolation_level);
-                     table.lookup1({static_cast<YCSBKey>(i)}, [&](const KVTable& record) { result = record.my_payload; });
-                     // cr::Worker::my().commitTX();
-                  }
-               });
-            });
-            crm.joinAll();
-            end = chrono::high_resolution_clock::now();
+
+   cout << "Inserting " << ycsb_tuple_count << " values" << endl;
+   begin = chrono::high_resolution_clock::now();
+
+   // this is the old parallelize loop
+
+   // comment: this is for insertion and therefore only one big loop without
+   // switches... so no multiple schedule here
+
+   // 1. it parallelizes the workload over the threads
+   // 2. the threads (worker) work on them one by one -> no spikes here
+
+   // what we want to change
+   // 1. workload distribution is handled not by us but by osv
+   // 2. the task creation returns instantly (because it just gets enqueued in the loop) -> how to handle load balancing
+
+   /* utils::Parallelize::range(FLAGS_ycsb_insert_threads ? FLAGS_ycsb_insert_threads : FLAGS_worker_threads, n, [&](u64 t_i, u64 begin, u64 end) {
+      crm.scheduleJobAsync(t_i, [&, begin, end]() {
+         for (u64 i = begin; i < end; i++) {
+            YCSBPayload payload;
+            utils::RandomGenerator::getRandString(reinterpret_cast<u8*>(&payload), sizeof(YCSBPayload));
+            YCSBKey key = i;
+            cr::Worker::my().startTX(tx_type, leanstore::TX_ISOLATION_LEVEL::SNAPSHOT_ISOLATION);
+            table.insert({key}, {payload});
+            cr::Worker::my().commitTX();
          }
-         // -------------------------------------------------------------------------------------
-         cout << "time elapsed = " << (chrono::duration_cast<chrono::microseconds>(end - begin).count() / 1000000.0) << endl;
-         cout << calculateMTPS(begin, end, n) << " M tps" << endl;
-         cout << "-------------------------------------------------------------------------------------" << endl;
-      }
-   } else {
-      cout << "Inserting " << ycsb_tuple_count << " values" << endl;
-      begin = chrono::high_resolution_clock::now();
-      utils::Parallelize::range(FLAGS_ycsb_insert_threads ? FLAGS_ycsb_insert_threads : FLAGS_worker_threads, n, [&](u64 t_i, u64 begin, u64 end) {
-         crm.scheduleJobAsync(t_i, [&, begin, end]() {
-            for (u64 i = begin; i < end; i++) {
-               YCSBPayload payload;
-               utils::RandomGenerator::getRandString(reinterpret_cast<u8*>(&payload), sizeof(YCSBPayload));
-               YCSBKey key = i;
-               cr::Worker::my().startTX(tx_type, leanstore::TX_ISOLATION_LEVEL::SNAPSHOT_ISOLATION);
-               table.insert({key}, {payload});
-               cr::Worker::my().commitTX();
-            }
-         });
       });
-      crm.joinAll();
-      end = chrono::high_resolution_clock::now();
-      cout << "time elapsed = " << (chrono::duration_cast<chrono::microseconds>(end - begin).count() / 1000000.0) << endl;
-      cout << calculateMTPS(begin, end, n) << " M tps" << endl;
-      // -------------------------------------------------------------------------------------
-      const u64 written_pages = db.getBufferManager().consumedPages();
-      const u64 mib = written_pages * PAGE_SIZE / 1024 / 1024;
-      cout << "Inserted volume: (pages, MiB) = (" << written_pages << ", " << mib << ")" << endl;
-      cout << "-------------------------------------------------------------------------------------" << endl;
+   });
+
+   crm.joinAll(); */
+
+   // in the osv way we probably send one big task that would be get converted to a thread
+   // and just wait until that is done
+   // we will check that with a semphore
+
+   // for now we wont do that at all because we do not use a lot of threads and therefore
+   // the parallelization is not important -> it also would be interesting on how to work with this
+   // parallelization in the future? should we build other abstractions for it? parallel for loop for osv tasks
+
+   // FIXME: in the future this should be distributed to multiple threads
+   // FIXME: WAL things are currently not used
+   for (u64 i = 0; i < n; i++) {
+      YCSBPayload payload;
+      utils::RandomGenerator::getRandString(reinterpret_cast<u8*>(&payload), sizeof(YCSBPayload));
+      YCSBKey key = i;
+      // cr::Worker::my().startTX(tx_type, leanstore::TX_ISOLATION_LEVEL::SNAPSHOT_ISOLATION);
+      table.insert({key}, {payload});
+      WorkerCounters::myCounters().tx++;
+      // cr::Worker::my().commitTX();
    }
+
+   end = chrono::high_resolution_clock::now();
+   cout << "time elapsed = " << (chrono::duration_cast<chrono::microseconds>(end - begin).count() / 1000000.0) << endl;
+   cout << calculateMTPS(begin, end, n) << " M tps" << endl;
+   // -------------------------------------------------------------------------------------
+   const u64 written_pages = db.getBufferManager().consumedPages();
+   const u64 mib = written_pages * PAGE_SIZE / 1024 / 1024;
+   cout << "Inserted volume: (pages, MiB) = (" << written_pages << ", " << mib << ")" << endl;
+   cout << "-------------------------------------------------------------------------------------" << endl;
    // -------------------------------------------------------------------------------------
    auto zipf_random = std::make_unique<utils::ScrambledZipfGenerator>(0, ycsb_tuple_count, FLAGS_zipf_factor);
    cout << setprecision(4);
@@ -146,6 +158,15 @@ int main(int argc, char** argv)
    atomic<bool> keep_running = true;
    atomic<u64> running_threads_counter = 0;
    const u32 exec_threads = FLAGS_ycsb_threads ? FLAGS_ycsb_threads : FLAGS_worker_threads;
+
+   // this is the loop we want to figure out on how to implement it in osv
+   // problem is that in our case we just run them in order
+   // now we are putting them in some runqueue and run them
+   // -> this is just what is possible if we run it on full throttle but is not in any case realistic
+   // TODO: how to translate it in leanstore?
+   //          - for now we can just execute them in a loop with a nsleep in between
+
+#ifdef OSV
    for (u64 t_i = 0; t_i < exec_threads - ((FLAGS_ycsb_sleepy_thread) ? 1 : 0); t_i++) {
       crm.scheduleJobAsync(t_i, [&]() {
          running_threads_counter++;
@@ -163,51 +184,70 @@ int main(int argc, char** argv)
                cr::Worker::my().startTX(tx_type, isolation_level);
                for (u64 op_i = 0; op_i < FLAGS_ycsb_ops_per_tx; op_i++) {
                   if (FLAGS_ycsb_read_ratio == 100 || utils::RandomGenerator::getRandU64(0, 100) < FLAGS_ycsb_read_ratio) {
-                     table.lookup1({key}, [&](const KVTable&) {});  // result = record.my_payload;
+                     table.lookup1({key}, [&](const KVTable&) {});         // result = record.my_payload;
                      leanstore::storage::BMC::global_bf->evictLastPage();  // to ignore the replacement strategy effect on MVCC experiment
                   } else {
                      UpdateDescriptorGenerator1(tabular_update_descriptor, KVTable, my_payload);
                      utils::RandomGenerator::getRandString(reinterpret_cast<u8*>(&result), sizeof(YCSBPayload));
                      // -------------------------------------------------------------------------------------
-                     table.update1(
-                         {key}, [&](KVTable& rec) { rec.my_payload = result; }, tabular_update_descriptor);
+                     table.update1({key}, [&](KVTable& rec) { rec.my_payload = result; }, tabular_update_descriptor);
                      leanstore::storage::BMC::global_bf->evictLastPage();  // to ignore the replacement strategy effect on MVCC experiment
                   }
                }
                cr::Worker::my().commitTX();
                WorkerCounters::myCounters().tx++;
             }
-            jumpmuCatch() { WorkerCounters::myCounters().tx_abort++; }
-         }
-         running_threads_counter--;
-      });
-   }
-   // -------------------------------------------------------------------------------------
-   if (FLAGS_ycsb_sleepy_thread) {
-      const leanstore::TX_MODE tx_type = FLAGS_olap_mode ? leanstore::TX_MODE::OLAP : leanstore::TX_MODE::OLTP;
-      crm.scheduleJobAsync(exec_threads - 1, [&]() {
-         running_threads_counter++;
-         while (keep_running) {
-            jumpmuTry()
+            jumpmuCatch()
             {
-               cr::Worker::my().startTX(tx_type, leanstore::TX_ISOLATION_LEVEL::SNAPSHOT_ISOLATION);
-               sleep(FLAGS_ycsb_sleepy_thread);
-               cr::Worker::my().commitTX();
+               WorkerCounters::myCounters().tx_abort++;
             }
-            jumpmuCatch() {}
          }
          running_threads_counter--;
       });
    }
-   // -------------------------------------------------------------------------------------
-   {
-      // Shutdown threads
-      sleep(FLAGS_run_for_seconds);
-      keep_running = false;
-      while (running_threads_counter) {
+
+#else
+   struct YCSBArgs {
+      LeanStoreAdapter<KVTable>* table;
+      YCSBKey key;
+   };
+
+   while (keep_running) {
+      jumpmuTry()
+      {
+         YCSBKey key;
+         if (FLAGS_zipf_factor == 0) {
+            key = utils::RandomGenerator::getRandU64(0, ycsb_tuple_count);
+         } else {
+            key = zipf_random->rand();
+         }
+         assert(key < ycsb_tuple_count);
+         YCSBPayload result;
+         YCSBArgs* a = new YCSBArgs{&table, key};
+         // cr::Worker::my().startTX(tx_type, isolation_level);
+         for (u64 op_i = 0; op_i < FLAGS_ycsb_ops_per_tx; op_i++) {
+            osv_task_enqueue(
+                [](void* args) {
+                   YCSBArgs* ycsb_args = (YCSBArgs*)args;
+
+                   ycsb_args->table->lookup1({ycsb_args->key}, [&](const KVTable&) {});  // result = record.my_payload;
+                   leanstore::storage::BMC::global_bf->evictLastPage();  // to ignore the replacement strategy effect on MVCC experiment
+                }, a);
+                usleep(10000); 
+         }
+         // cr::Worker::my().commitTX();
+         WorkerCounters::myCounters().tx++;
       }
-      crm.joinAll();
+      jumpmuCatch()
+      {
+         WorkerCounters::myCounters().tx_abort++;
+      }
    }
+#endif
+   // -------------------------------------------------------------------------------------
+
+   // -------------------------------------------------------------------------------------
+
    cout << "-------------------------------------------------------------------------------------" << endl;
    return 0;
 }
