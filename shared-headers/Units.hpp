3f2d548555420338654916ca3c48a395956f65f4
kosakseb <sebastian.kosak@tum.de>
2025-02-18 14:31:15 +0100

    increase MAX_CORES from 4 to 12 to support higher concurrency

--- shared-headers/Units.hpp
+++ shared-headers/Units.hpp
@@ -40,7 +40,7 @@ using WORKERID = u16;
 using TXID = u64;
 using COMMANDID = u32;
 #define TYPE_MSB(TYPE) (1ull << ((sizeof(TYPE) * 8) - 1))
-#define MAX_CORES 4
+#define MAX_CORES 12
 // -------------------------------------------------------------------------------------
 using TINYINT = s8;
 using SMALLINT = s16;
