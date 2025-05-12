de9d9d98360f6c424ef70f2eb9ff465c0223465d
Sebastian Kosak <sebastian.kosak@tum.de>
2025-05-12 17:35:51 +0200

    fixed the debug outstanding counter of RequestStackLock.hpp

--- backend/leanstore/io/RequestStackLock.hpp
+++ backend/leanstore/io/RequestStackLock.hpp
@@ -22,13 +22,11 @@ public:
     std::unique_ptr<R*[]> free_stack;
     std::unique_ptr<R*[]> submit_stack;
     
-#if false
-    std::unordered_set<R*> outstanding_set;
-#endif
     const int max_entries;
     std::atomic<int> free;
     std::atomic<int> pushed = {0};
-    
+    std::atomic<int> outstanding_cnt = {0};
+
     // Mutexes for thread-safety
     std::mutex free_stack_mutex;
     std::mutex submit_stack_mutex;
@@ -48,7 +46,7 @@ public:
     
     int outstanding()
     {
-        assert(max_entries - free - pushed == outstanding_set.size());
+        assert(max_entries - free.load() - pushed.load() == outstanding_cnt.load());
         return max_entries - free.load() - pushed.load();
     }
     
@@ -105,40 +103,11 @@ public:
     }
     
     /* submit -> outstanding */
-    void emptySubmitStack()
-    {
-#if false
-        // Need to lock both submit stack and the outstanding set
-        std::lock(submit_stack_mutex, outstanding_mutex);
-        std::lock_guard<std::mutex> submit_lock(submit_stack_mutex, std::adopt_lock);
-        std::lock_guard<std::mutex> outstanding_lock(outstanding_mutex, std::adopt_lock);
-        
-        for (int i = 0; i < pushed; i++) {
-            auto found = outstanding_set.find(submit_stack[i]);
-            if (found == outstanding_set.end()) {
-                outstanding_set.insert(submit_stack[i]);
-            }
-            submit_stack[i] = nullptr;
-        }
-#else
-        std::lock_guard<std::mutex> submit_lock(submit_stack_mutex);
-        for (int i = 0; i < pushed; i++) {
-            submit_stack[i] = nullptr;
-        }
-#endif
-        pushed = 0;
-    }
-    
-    /* submit -> outstanding */
     bool popFromSubmitStack(R*& out)
     {
-#if false
         std::lock(submit_stack_mutex, outstanding_mutex);
         std::lock_guard<std::mutex> submit_lock(submit_stack_mutex, std::adopt_lock);
         std::lock_guard<std::mutex> outstanding_lock(outstanding_mutex, std::adopt_lock);
-#else
-        std::lock_guard<std::mutex> submit_lock(submit_stack_mutex);
-#endif
         
         if (pushed <= 0) {
             return false;
@@ -146,32 +115,19 @@ public:
         pushed--;
         out = submit_stack[pushed];
         
-#if false
-        auto found = outstanding_set.find(out);
-        // ensure(found == outstanding_set.end());
-        if (found == outstanding_set.end()) {
-            outstanding_set.insert(submit_stack[pushed]);
-        }
-#endif
+        outstanding_cnt++; 
+
         return true;
     }
     
     /* outstanding -> free */
     void returnToFreeList(R* ptr)
     {
-#if false
-        // Need to lock both free stack and the outstanding set
         std::lock(free_stack_mutex, outstanding_mutex);
         std::lock_guard<std::mutex> free_lock(free_stack_mutex, std::adopt_lock);
         std::lock_guard<std::mutex> outstanding_lock(outstanding_mutex, std::adopt_lock);
         
-        auto found = outstanding_set.find(ptr);
-        ensure(found != outstanding_set.end());
-        outstanding_set.erase(found);
-#else
-        std::lock_guard<std::mutex> free_lock(free_stack_mutex);
-#endif
-        
+        outstanding_cnt--;         
         free_stack[free] = ptr;
         free++;
     }
