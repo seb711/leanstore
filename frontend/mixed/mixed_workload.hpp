6f8522fb4a6ce62cf8c849f1f938450074b417e4
Sebastian Kosak <sebastian.kosak@tum.de>
2025-07-09 10:22:11 +0200

    feat: add mixed workload implementation with LeanStoreAdapter and related classes

---
+++ frontend/mixed/mixed_workload.hpp
@@ -0,0 +1,69 @@
+#include "types.hpp"
+#include "leanstore/LeanStore.hpp"
+
+#define CYCLE 10000
+#define PROBABLITY 5000
+#define LONGRUNNING 1
+#define MAX_ENTRIES 1000000000
+
+class Workload {
+   private: 
+      utils::ScrambledZipfGenerator zipf_random; 
+      std::atomic<unsigned> current_idx;
+      std::atomic<unsigned> highest_inserted;
+      utils::MersenneTwister twister; 
+      LeanStoreAdapter<item_t>& kv_store;
+      std::atomic<uint64_t> last_scan; 
+
+
+      void lookupTblRnd(uint64_t w_id) {
+         uint64_t key = zipf_random.rand();
+         BytesPayload<120> result;  /// FIXME remove this check
+         kv_store.lookup1({w_id}, [&](const item_t& item) { result = item.i_data; });
+      }
+      // we need one easy look up
+      void writeTblSeq() {
+         BytesPayload<120> payload;
+         utils::RandomGenerator::getRandString(reinterpret_cast<u8*>(&payload), sizeof(BytesPayload<120>));
+         auto key = current_idx.fetch_add(1);
+         kv_store.insert({key}, {payload});
+         // BytesPayload<120> result;  /// FIXME remove this check
+         // kv_store.lookup1({key}, [&](const item_t& item) { result = item.i_data; });
+
+         if (highest_inserted.load() < key) {
+            highest_inserted.store(key);
+         }
+
+      }
+      // we need one scan
+      void scanSeqTbl() {
+         unsigned curr = highest_inserted.load(); 
+         BytesPayload<120> result;  /// FIXME remove this check
+         for (uint64_t t = 10; t < CYCLE; t++) {
+            kv_store.lookup1({curr - t}, [&](const item_t& item) { result = item.i_data; });
+         }
+      }
+
+   public: 
+
+      Workload(LeanStoreAdapter<item_t>& kv_store) : zipf_random(0, MAX_ENTRIES, FLAGS_zipf_factor),  current_idx(0), highest_inserted(0), twister(), kv_store(kv_store), last_scan(mean::readTSC()) {}; 
+
+      int tx() {
+         if (mean::tscDifferenceMs(mean::readTSC(), last_scan) > 500) {
+             last_scan.store(mean::readTSC()); 
+             scanSeqTbl(); 
+             return 1; 
+         } else {
+            writeTblSeq(); 
+            return 0; 
+         }
+      }
+
+      int insert() {
+         if (current_idx.load() % 10000 == 0) {
+            std::cout << current_idx.load() << std::endl; 
+         }
+         writeTblSeq(); 
+         return 0; 
+      }
+}; 
