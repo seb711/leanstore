d027c50fb1f710b59b870d1b58cdd9f365aad9af
Sebastian Kosak <sebastian.kosak@tum.de>
2025-07-25 17:11:17 +0200

    feat: add support again for io_uring engine in IoInterface

--- backend/leanstore/io/IoInterface.cpp
+++ backend/leanstore/io/IoInterface.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 // -------------------------------------------------------------------------------------
 #include "impl/LibaioImpl.hpp"
+#include "impl/LiburingImpl.hpp"
 #include "impl/SpdkImpl.hpp"
 #include "impl/XnvmeImpl.hpp"
 #include "impl/OsvImpl.hpp"
@@ -20,6 +21,8 @@ RaidEnvironment& IoInterface::initInstance(IoOptions ioOptions)
    } else if (ioOptions.engine == "spdk") {
       _instance = std::unique_ptr<RaidEnvironment>(new RaidEnv<SpdkEnv, SpdkChannel, SpdkIoReq>(ioOptions));
 #endif
+   } else if (ioOptions.engine == "io_uring") {
+      _instance = std::unique_ptr<RaidEnvironment>(new RaidEnv<LiburingEnv, LiburingChannel, LiburingIoRequest>(ioOptions));
 #ifdef LEANSTORE_INCLUDE_OSV
    } else if (ioOptions.engine == "osv") {
       _instance = std::unique_ptr<RaidEnvironment>(new RaidEnv<OsvEnv, OsvChannel, OsvIoReq>(ioOptions));
