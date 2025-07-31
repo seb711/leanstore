d05d69115ac4d41f560696b126fce4ba7c4c79a8
Sebastian Kosak <sebastian.kosak@tum.de>
2025-07-31 12:20:52 +0200

    refactor: update mutex usage and enhance jumpmu handling in resolveSwip

--- backend/leanstore/storage/buffer-manager/BufferManager.cpp
+++ backend/leanstore/storage/buffer-manager/BufferManager.cpp
@@ -257,6 +257,7 @@ void BufferManager::reclaimPage(BufferFrame& bf)
    }
 }
 // -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
 // returns a non-latched BufferFrame
 BufferFrame& BufferManager::resolveSwip(Guard& swip_guard, Swip<BufferFrame>& swip_value)
 {
@@ -303,10 +304,11 @@ BufferFrame& BufferManager::resolveSwip(Guard& swip_guard, Swip<BufferFrame>& sw
    swip_guard.unlock();  // otherwise we would get a deadlock, P->G, G->P
    const PID pid = swip_value.asPageID();
    IoPartition& partition = getIoPartition(pid);
-   JMUW<std::unique_lock<mean::mutex>> g_guard(partition.io_mutex);
+   JMUW<std::unique_lock<mean::io_mutex>> g_guard(partition.io_mutex);
    swip_guard.recheck();
    assert(!swip_value.isHOT());
    // -------------------------------------------------------------------------------------
+   leanstore_osv_debug::trace_finish_transaction(pid); 
    auto frame_handler = partition.io_ht.lookup(pid);
    if (!frame_handler) {
       BufferFrame& bf = randomCoolingPartition().dram_free_list.tryPop(g_guard);  // EXP
@@ -376,7 +378,7 @@ BufferFrame& BufferManager::resolveSwip(Guard& swip_guard, Swip<BufferFrame>& sw
             setOptimisticParentPointer(swip_guard, swip_value);
          }
          // -------------------------------------------------------------------------------------
-         if (io_frame.readers_counter.fetch_add(-1) == 1) {
+          if (io_frame.readers_counter.fetch_add(-1) == 1) {
             partition.io_ht.remove(pid);
          }
          // -------------------------------------------------------------------------------------
@@ -390,10 +392,14 @@ BufferFrame& BufferManager::resolveSwip(Guard& swip_guard, Swip<BufferFrame>& sw
          io_frame.bf = &bf;
          io_frame.state = IOFrame::STATE::READY;
          // -------------------------------------------------------------------------------------
-         g_guard->unlock();
          io_frame.mutex.unlock();
+         g_guard->unlock();
          // -------------------------------------------------------------------------------------
-         jumpmu::jump();
+         if (jumpmu::user_jump_reason() == jumpmu::UserJumpReason::Reason3) {
+            jumpmu::jump(jumpmu::UserJumpReason::Reason3);
+         } else {
+            jumpmu::jump(jumpmu::UserJumpReason::Reason1);
+         }
       }
    }
    // -------------------------------------------------------------------------------------
@@ -412,13 +418,14 @@ BufferFrame& BufferManager::resolveSwip(Guard& swip_guard, Swip<BufferFrame>& sw
          g_guard->unlock();
       }
       // -------------------------------------------------------------------------------------
-      jumpmu::jump();
+      jumpmu::jump(jumpmu::UserJumpReason::Reason2);
    }
    // -------------------------------------------------------------------------------------
    if (io_frame.state == IOFrame::STATE::READY) {
       // -------------------------------------------------------------------------------------
       BufferFrame* volatile bf = io_frame.bf;
       {
+         // io_frame.readers_counter++;  // incremented while holding partition lock
          // We have to exclusively lock the bf because the page provider thread will
          // try to evict them when its IO is done
          bf->header.latch.assertNotExclusivelyLatched();
@@ -443,6 +450,7 @@ BufferFrame& BufferManager::resolveSwip(Guard& swip_guard, Swip<BufferFrame>& sw
             std::cout << "frame: " << io_frame.readers_counter << " bf.dt_id: " << bf->page.dt_id<< std::endl;
             //raise(SIGINT);
          }
+         
          if (io_frame.readers_counter.fetch_add(-1) == 1) {
             partition.io_ht.remove(pid);
          } else {
@@ -458,7 +466,7 @@ BufferFrame& BufferManager::resolveSwip(Guard& swip_guard, Swip<BufferFrame>& sw
          partition.io_ht.remove(pid);
       }
       g_guard->unlock();
-      jumpmu::jump();
+      jumpmu::jump(jumpmu::UserJumpReason::Reason3);
    }
    ensure(false);
 #pragma GCC diagnostic push
