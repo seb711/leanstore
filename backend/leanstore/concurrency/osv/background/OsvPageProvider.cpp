06828e8c3fc861716e2ec13f2025b0c76030b797
Sebastian Kosak <sebastian.kosak@tum.de>
2025-08-12 15:02:57 +0200

    add page provider security checks for concurreny issues if too many threads block on io_mutex then the pageprovider starves due to osv lock impl

--- backend/leanstore/concurrency/osv/background/OsvPageProvider.cpp
+++ backend/leanstore/concurrency/osv/background/OsvPageProvider.cpp
@@ -1,4 +1,7 @@
 #include "OsvPageProvider.hpp"
+#include <osv/leanstore_debug.hh> 
+#include "leanstore/Config.hpp"
+#include <gflags/gflags.h>
 
 namespace mean
 {
@@ -17,15 +20,48 @@ unsigned OsvPageProvider::getPriority() {
    // return bf_ptr->cooling_partitions[partition_id].dram_free_list.counter < 100 ? 1 : 0; 
 
    leanstore_osv_debug::trace_pageprovider_state(counter); 
-   return (counter < 1000) ? 1 : 0; 
+   return (counter < 250) ? 1 : 0; 
 }
 
 int OsvPageProvider::process()
 {
+
    while (true) {
          // std::cout << "empty freelist " << bf_ptr->cooling_partitions[partition_id].dram_free_list.counter << std::endl; 
          leanstore_osv_debug::trace_background_result(bf_ptr->cooling_partitions[partition_id].dram_free_list.counter); 
+         // bf_ptr->pageProviderCycle(partition_id);
+
+/*
+               THIS IS JUST A TEMPORARY FIX FOR A SITUATION IN WHICH THE PAGEPROVIDER
+               CANNOT ACCESS THE LOCKS DUE TO HOW LOCKS ARE IMPLEMENTED IN OSV
+
+               - THE JOB THREADS WAIT FOR FREE PAGES AND REQUEST A LOCK AND THEREFORE
+               INCREMENT THE LOCK-COUNTER IN LFMUTEX.CC
+               - THE PAGEPROVIDER ALSO WANTS THE LOCK TO FREE PAGES; BUT THE PAGEPROVIDER
+               DOES THIS WITH TRY-LOCK AND NOT WITH LOCK AND THEREFORE HAS LEAST PRIORITY
+
+               WE CURRENTLY RESOLVE THIS BY ASSESSING WHEN THIS SITUATION IS ACTIVE (NO FREE PAGES AND THE SITUATION IS NOT HANDLED)
+
+               - IN THAT CASE WE GET LOCKS ON ALL IO PARTITIONS AND CALL THE PAGEPROVIDER
+
+               THIS SOLUTION IS CURRENTLY ONLY POSSIBLE IF WE HAVE ONE COOLING PARTITION
+               FIXME: ADD SUPPORT FOR MULTIPLE COOLING PARTITIONS
+            */
+            if (bf_ptr->cooling_partitions[partition_id].dram_free_list.counter == 0 && counter++ > 10) {
+               std::cout << "pageprovider situation" << std::endl; 
+               std::vector<std::unique_ptr<std::unique_lock<mean::io_mutex>>> locks;
+
+               for (size_t io_partition_idx = 0; io_partition_idx < bf_ptr->io_partitions_count; io_partition_idx++) {
+                  locks.push_back(std::make_unique<std::unique_lock<mean::io_mutex>>(bf_ptr->io_partitions[io_partition_idx].io_mutex));
+               }
+
+               bf_ptr->pageProviderCycle(partition_id);
+
+               counter = 0;
+            } else {
          bf_ptr->pageProviderCycle(partition_id);
+
+            }
          // assert(bf_ptr->cooling_partitions[partition_id].dram_free_list.counter > 1000); 
 
       leanstore_osv_debug::yield(); 
