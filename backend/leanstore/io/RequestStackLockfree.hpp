8a2ce1f608e654238105484c1bfdcd1f80080721
Sebastian Kosak <sebastian.kosak@tum.de>
2025-08-18 10:56:45 +0200

    also switch in the requeststacklockfree queue to boost implementation to be sure; this is just to avoid issues with the memory

--- backend/leanstore/io/RequestStackLockfree.hpp
+++ backend/leanstore/io/RequestStackLockfree.hpp
@@ -2,12 +2,9 @@
 // -------------------------------------------------------------------------------------
 #include <chrono>
 #include <memory>
-#include <mutex>
 #include <stdexcept>
 #include <unordered_set>
-#include <boost/lockfree/stack.hpp>
 #include <boost/lockfree/queue.hpp>
-#include <boost/lockfree/spsc_queue.hpp>
 #include <boost/atomic.hpp>
 #include "Exceptions.hpp"
 #include "leanstore/io/IoRequest.hpp"
@@ -15,18 +12,17 @@
 namespace mean
 {
 // -------------------------------------------------------------------------------------
-// NOTE: CURRENTLY THE REQUESTSTACK IS USED AS A THREAD-SAFE DATASTRUCTURE
-//       THEREFORE I ADDED A MUTEX -> THIS SHOULD BE CHANGED IN THE FUTURE
 template <typename R>
 class RequestStackLockfree
 {
 public:
     std::unique_ptr<R[]> requests;
-    // boost::lockfree::stack<R*> free_stack;
-    std::atomic<R*> head = {}; 
     
-    // Note: Boost doesn't have a lockfree set, but we can use a concurrent_set from TBB
-    // or implement our own atomic-based tracking for debug purposes
+    // Using boost lockfree queue instead of manual atomic linked list
+    // Fixed capacity queue for better performance
+    boost::lockfree::queue<R*, boost::lockfree::fixed_sized<true>> free_queue;
+    
+    // Track outstanding requests for debugging
     std::atomic<int> outstanding_count{0};
     
     const int max_entries;
@@ -34,21 +30,28 @@ public:
     std::atomic<int> pushed{0};
     
     RequestStackLockfree(int max_entries) : 
-        max_entries(max_entries),
-        free(max_entries)
+        free_queue(max_entries),  // Initialize with fixed capacity
+                max_entries(max_entries),
+                        free(max_entries)
+
     {
         requests = std::make_unique<R[]>(max_entries);
+        
+        // Push all requests to the free queue initially
         for (int i = 0; i < max_entries; i++) {
-            requests[i].impl.next = head.load(); 
-            head.store(&requests[i]); 
+            bool success = free_queue.push(&requests[i]);
+            // In constructor, this should always succeed since queue is empty
+            assert(success && "Failed to initialize free queue");
         }
     }
     
-    ~RequestStackLockfree() {}
+    ~RequestStackLockfree() {
+        // Queue will be destroyed automatically
+        // Requests array will be destroyed by unique_ptr
+    }
     
     int outstanding()
     {
-        // ensure(max_entries - free.load() - pushed.load() == outstanding_count.load());
         return outstanding_count.load();
     }
     
@@ -59,42 +62,42 @@ public:
     /* free -> to user (untracked) */
     bool popFromFreeStack(R*& out)
     {
-        // ensure(free.load() >= 0);
         if (free.load() == 0) {
             return false;
         }
         
-        R* old_top = head.load();
-        
-        while (old_top && !head.compare_exchange_weak(old_top, old_top->impl.next)) {
-            // CAS failed, retry with updated old_top
-            // compare_exchange_weak updates old_top on failure
+        R* ptr = nullptr;
+        if (free_queue.pop(ptr)) {
+            free.fetch_sub(1);
+            outstanding_count.fetch_add(1);
+            out = ptr;
+            return true;
         }
         
-        if (!old_top) {
-            return false; // Stack was empty
-        }
-        
-        free.fetch_sub(1);
-        outstanding_count.fetch_add(1);
-        out = old_top;
-        return true;
+        return false;
     }
     
     /* outstanding -> free */
     void returnToFreeList(R* ptr)
     {
         outstanding_count.fetch_sub(1);
-
-        R* old_top; 
-        do {
-            old_top = head.load(); 
-            ptr->impl.next = old_top;
-        } while (!head.compare_exchange_weak(old_top, ptr)); 
-
+        
+        bool success = free_queue.push(ptr);
+        if (!success) {
+            // This should not happen if the logic is correct
+            // (we can't return more than max_entries)
+            throw std::runtime_error("Failed to return request to free queue - queue full");
+        }
+        
         free.fetch_add(1);
     }
+    
+    // Alternative: Get approximate size of free queue (may be useful for debugging)
+    // Note: This is not exact in concurrent scenarios
+    size_t approximateFreeSize() const {
+        // boost::lockfree::queue doesn't provide a size() method that's lock-free
+        // We're using the atomic 'free' counter instead
+        return free.load();
+    }
 };
-// -------------------------------------------------------------------------------------
-}  // namespace mean
-// -------------------------------------------------------------------------------------
+}
